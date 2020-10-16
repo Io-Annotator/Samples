1251a688afe001235c863cc6333eccd4579f78f7
Sascha Heyer <heyersaschastud@googlemail.com>
2020-10-16 09:44:59 +0200

    added link to sample csv

--- python/text/import-csv.py
+++ python/text/import-csv.py
@@ -5,6 +5,7 @@ import csv
 params= {'apikey': 'add your API key here'}
 api = 'https://api.ioannotator.com/import/texts'
 
+# sample csv can be found here https://github.com/Io-Annotator/Samples/blob/main/python/text/sample.csv
 with open('./sample.csv') as csvfile:
     rows = csv.DictReader(csvfile, delimiter=';')
 
